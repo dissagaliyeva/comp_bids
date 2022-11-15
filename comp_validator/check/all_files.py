f8758c116fcefa7115ac7ee4696095c4fc921922
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-11-15 16:01:49 +0400

    Find location of nodes/labels and check destination folder

--- comp_validator/check/all_files.py
+++ comp_validator/check/all_files.py
@@ -16,6 +16,8 @@ Nx3_dim = ['nodes', 'vertices', 'vnormals', 'fnormals', 'sensors', 'orientations
 NxM_dim = ['map', 'faces']
 TxN_dim = ['vars', 'stimuli', 'noise', 'raster', 'emp', 'ts', 'events']
 
+COORDS = ['weights', 'distances', 'delays', 'speeds', 'emp', 'ts', 'events', 'fc', 'vars', 'stimuli', 'noise', 'raster']
+
 
 class Files:
     def __init__(self, path):
@@ -25,11 +27,45 @@ class Files:
         # check all json files
         self.check_files()
 
-    def check_files(self):
+        # check where nodes/labels are
+        self.coords = None
+        self.coords_is_global = None
+        self.find_coords()
+
+    def find_coords(self):
+        coords = []
+
         for file in self.content:
+            if 'nodes' in file:
+                coords.append(file)
+            if 'labels' in file:
+                coords.append(file)
+
+        if coords:
+            self.coords = coords
+            path, basename = os.path.dirname(coords[0]), os.path.basename(coords[0])
+
+            if 'sub-' not in coords[0].lower():
+                p = os.path.basename(os.path.dirname(coords[0]))
+
+                if os.path.exists(os.path.join(path, 'nodes.json')) or os.path.exists(os.path.join(path, 'labels.json')) \
+                    or os.path.exists(os.path.join(path,'nodes.tsv')) or os.path.exists(os.path.join(path, 'labels.tsv')):
+                    self.coords_is_global = True
+                else:
+                    utils.add_error(21, p, basename, f'{", ".join(self.coords)} must be in the global `coord` folder.')
+            else:
+                self.coords_is_global = False
+
+
+    def check_files(self):
+        desc = '{} has {}x{} dimensions. Expected to see {}x{} dimensions.'
+        coord_desc = '{} does not have the required field `CoordsRows` or `CoordsColumns`.'
+
+        for file in self.content:
+            path, basename = os.path.dirname(file), os.path.basename(file)
+
             if file.endswith('json'):
                 jfile = json.load(open(file))
-                path, basename = os.path.dirname(file), os.path.basename(file)
 
                 # check if Description field is present
                 if 'Description' not in jfile.keys():
@@ -41,8 +77,6 @@ class Files:
                     check_rows_columns(jfile, path, basename)
 
                 # check dimensions
-
-                desc = '{} has {}x{} dimensions. Expected to see {}x{} dimensions.'
                 for idx, (dims, dim) in enumerate(zip([NxN_dim, Nx1_dim, Nx2_dim, Nx3_dim, NxM_dim],
                                                       ['n', 1, 2, 3, 'm'])):
                     if get_rows_columns(file, dims):
@@ -62,6 +96,40 @@ class Files:
                             utils.add_error(19, path, basename, desc.format(basename, rows, columns, 't', 'n'))
                     else:
                         check_rows_columns(jfile, path, basename)
+
+                # check if CoordsRows and CoordsColumns are present
+                if get_rows_columns(file, COORDS):
+                    if 'CoordsRows' in jfile.keys() and 'CoordsColumns' in jfile.keys():
+                        rows, columns = jfile['CoordsRows'], jfile['CoordsColumns']
+
+                        if not isinstance(rows, list) or not isinstance(columns, list):
+                            utils.add_error(20, path, basename,
+                                            f'{basename}\'s `CoordsRows` and `CoordsColumns` must be of type list.')
+                    else:
+                        utils.add_error(18, path, basename, coord_desc.format(basename))
+
+                # check coordinates folder
+                if 'coord' in file:
+                    # TODO: check where global nodes/labels are located
+                    pass
+
+
+
+
+            if file.endswith('.tsv'):
+                # check dimensions
+                pfile = pd.read_csv(file, sep='\t', header=None)
+                rows, columns = pfile.shape
+
+                for idx, (dims, dim) in enumerate(zip([NxN_dim, Nx1_dim, Nx2_dim, Nx3_dim, NxM_dim], ['n', 1, 2, 3, 'm'])):
+                    if get_rows_columns(file, dims):
+                        if (dim == 'n' and rows != columns) or (dim != 'n' and rows == columns) or \
+                                (rows == 1):
+                            utils.add_error(19, path, basename, desc.format(basename, rows, columns, 'n', dim))
+
+                if get_rows_columns(file, TxN_dim) or (rows == 1 or columns == 1):
+                    if rows == columns:
+                        utils.add_error(19, path, basename, desc.format(basename, rows, columns, 't', 'n'))
 
 
 def check_rows_columns(jfile, path, basename):
--- comp_validator/issues/issues.py
+++ comp_validator/issues/issues.py
@@ -6,7 +6,7 @@ https://github.com/bids-standard/bids-validator/blob/1c9598538929e4c067ca714ccea
 
 ISSUE_LIST = {
     # All files that need to be fixed (severity='error')
-    1: dict(key='NOT_INCLUDED', severity='error', reason=''),
+    1: dict(key='NOT_INCLUDED', severity='error', reason='The required TSV or JSON file is missing.'),
     2: dict(key='README_FILE_MISSING', severity='error', reason='The required file `README` is missing or has a wrong extension (accepted extensions: `md`, `txt`, and `rst`). See <a href="https://bids-specification.readthedocs.io/en/stable/03-modality-agnostic-files.html#readme">Section 03 (Modality agnostic files)</a> of the BIDS specification.'),
     3: dict(key='PARTICIPANTS_FILE_MISSING', severity='warning', reason='The recommended file `participants.json` or `participants.tsv` is missing. See <a href="https://bids-specification.readthedocs.io/en/stable/03-modality-agnostic-files.html#participants-file">Section 03 (Modality agnostic file)</a> of the BIDS specification.'),
     4: dict(key='CHANGES_FILE_MISSING', severity='warning', reason='The recommended file `CHANGES` is missing. See <a href="https://bids-specification.readthedocs.io/en/stable/03-modality-agnostic-files.html#changes">Section 03 (Modality agnostic files)</a> of the BIDS specification.'),
@@ -25,5 +25,8 @@ ISSUE_LIST = {
     17: dict(key='DATASET_DESCRIPTION_MISSING_FIELDS', severity='warning', reason='The required file `dataset_description.json` has missing values. See <a href="https://bids-specification.readthedocs.io/en/stable/03-modality-agnostic-files.html#dataset_descriptionjson">Section 03 (Modality agnostic files)</a> of the BIDS specification.'),
     18: dict(key='JSON_FILE_ISSUES', severity='error', reason='The required file misses required fields in JSON file.'),
     19: dict(key='DIMENSIONS_MISMATCH', severity='error', reason='The required JSON file has a mismatch in dimensions.'),
+    20: dict(key='WRONG_TYPE', severity='error', reason='The required JSON file has a wrong field type.'),
+    21: dict(key='WRONG_DESTINATION', severity='error', reason='The required TSV or JSON file is incorrectly placed.'),
+
 
 }
